c5b8baa32c47bce4e7ea8d6754433c44589a3ea2
Adrianna Lewandowska <a.lewandowsk42@student.uw.edu.pl>
2019-06-16 17:33:10 +0200

    #21 bug fixed

--- update_data.R
+++ update_data.R
@@ -3,8 +3,8 @@ source(here::here("src/include_functions.R"))
 australia1=read.csv(here::here("australia_1317.csv"),header=TRUE,sep=';')
 usa1=read.csv(here::here("usa_1317.csv"),header=TRUE,sep=';')
 
-australia = australia1[which(australia1$Year == minyear | australia1$Year == maxyear),]
-usa = usa1[which(usa1$Year == minyear | usa1$Year == maxyear),]
+australia = australia1[which(australia1$Year >= minyear & australia1$Year <= maxyear),]
+usa = usa1[which(usa1$Year >= minyear & usa1$Year <= maxyear),]
 
 total_australia=sortTotal(australia)
 holiday_australia=sortTravelPurpose(australia,"Holiday")
