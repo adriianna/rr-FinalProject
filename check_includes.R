240be91d8623d884418a93aa77485ba94f559ce2
Adrianna Lewandowska <a.lewandowsk42@student.uw.edu.pl>
2019-06-18 23:48:35 +0200

    check_includes - package added

--- check_includes.R
+++ check_includes.R
@@ -1,4 +1,6 @@
 #include all neccessary packages
+install.packages("here")
+library(here)
 source(here::here("pkgs.R"))
 missing_pkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
 if(length(missing_pkgs) > 0) install.packages(missing_pkgs)
